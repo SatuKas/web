0fa50344ccfaa9373d83c371719c95fcdd035b37
arubaya <abi.arubaya@gmail.com>
2025-06-22 23:10:13 +0700

    feat(layout): update layout metadata and remove unused assets

--- config/app.ts
+++ config/app.ts
@@ -1,4 +1,16 @@
-export const APP_NAME = 'Acme Inc';
+import ImageMeta from '@/assets/image/image-meta.png';
+import LogoIcon from '@/assets/image/logo-icon.svg';
+import LogoText from '@/assets/image/logo-text.svg';
+
+export const APP_NAME = 'My Cool App';
+export const APP_DESCRIPTION = 'My Cool App Description';
+
+export const APP_LOGO = {
+  icon: LogoIcon.src,
+  text: LogoText.src,
+};
+
+export const APP_IMAGE_META = ImageMeta.src;
 
 export const AUTH_CONFIG = {
   signUp: true,
