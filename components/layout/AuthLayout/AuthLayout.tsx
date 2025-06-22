33cc370a5513456f501bf09ff5cc135e8a3508b4
arubaya <abi.arubaya@gmail.com>
2025-06-22 21:14:24 +0700

    feat(layout): enhance AuthLayout with image support and legal links

--- components/layout/AuthLayout/AuthLayout.tsx
+++ components/layout/AuthLayout/AuthLayout.tsx
@@ -1,12 +1,43 @@
 import Box from '@/components/ui/Box';
+import { Card, CardContent } from '@/components/ui/Card';
+import Stack from '@/components/ui/Stack';
+import { PRIVACY_PATH_URL, TERMS_PATH_URL } from '@/constants/routes';
+import Image from 'next/image';
+import Link from 'next/link';
 import { ReactNode } from 'react';
 
 interface AuthLayoutProps {
   children: ReactNode;
+  image?: any;
 }
 
-const AuthLayout = ({ children }: AuthLayoutProps) => {
-  return <Box>{children}</Box>;
+const AuthLayout = ({ children, image }: AuthLayoutProps) => {
+  return (
+    <Box className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <Box className="w-full max-w-sm md:max-w-3xl">
+        <Stack gap={6}>
+          <Card className="overflow-hidden p-0">
+            <CardContent className="grid p-0 md:grid-cols-2">
+              {children}
+              <Box className="bg-muted relative hidden md:block">
+                <Image
+                  src={image}
+                  alt="Image"
+                  width={500}
+                  height={500}
+                  className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                />
+              </Box>
+            </CardContent>
+          </Card>
+          <Box className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+            By clicking continue, you agree to our <Link href={TERMS_PATH_URL}>Terms of Service</Link> and{' '}
+            <Link href={PRIVACY_PATH_URL}>Privacy Policy</Link>.
+          </Box>
+        </Stack>
+      </Box>
+    </Box>
+  );
 };
 
 export default AuthLayout;
--- constants/routes.ts
+++ constants/routes.ts
@@ -1,9 +1,14 @@
 // Landing Page Routes
 export const LANDING_PAGE_PATH_URL = '/';
 
+// Legal Routes
+export const TERMS_PATH_URL = '/legal/terms';
+export const PRIVACY_PATH_URL = '/legal/privacy';
+
 // Auth Routes
 export const LOGIN_PATH_URL = '/auth/login';
 export const REGISTER_PATH_URL = '/auth/register';
+export const FORGOT_PASSWORD_PATH_URL = '/auth/forgot-password';
 
 // Dashboard Routes
 export const DASHBOARD_PATH_URL = '/dashboard';
