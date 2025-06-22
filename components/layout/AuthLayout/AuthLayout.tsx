eab54e87b344adf9e0a7632f44914b80aedaafe1
arubaya <abi.arubaya@gmail.com>
2025-06-22 22:48:12 +0700

    feat(auth): update LoginForm with social media login options and improve layout

--- components/layout/AuthLayout/AuthLayout.tsx
+++ components/layout/AuthLayout/AuthLayout.tsx
@@ -16,8 +16,8 @@ const AuthLayout = ({ children, image }: AuthLayoutProps) => {
     <Box className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <Box className="w-full max-w-sm md:max-w-3xl">
         <Stack gap={6}>
-          <Card className="overflow-hidden p-0">
-            <CardContent className="grid p-0 md:grid-cols-2">
+          <Card className="overflow-hidden p-0 min-h-[556px]">
+            <CardContent className="grid p-0 md:grid-cols-2 min-h-[556px]">
               {children}
               <Box className="bg-muted relative hidden md:block">
                 <Image
--- components/module/auth/LoginForm/LoginForm.tsx
+++ components/module/auth/LoginForm/LoginForm.tsx
@@ -1,18 +1,18 @@
 'use client';
 
-import AppleBrand from '@/assets/icon/apple-brand.svg';
-import GoogleBrand from '@/assets/icon/google-brand.svg';
-import MetaBrand from '@/assets/icon/meta-brand.svg';
+import AppleBrand from '@/assets/icon/AppleBrand';
+import GoogleBrand from '@/assets/icon/GoogleBrand';
+import MetaBrand from '@/assets/icon/MetaBrand';
 import Box from '@/components/ui/Box';
 import Button from '@/components/ui/Button';
 import Form, { FormField } from '@/components/ui/Form';
 import Input from '@/components/ui/Input';
 import Stack from '@/components/ui/Stack';
 import Typography from '@/components/ui/Typography';
+import { AUTH_CONFIG } from '@/config/app';
 import { FORGOT_PASSWORD_PATH_URL, REGISTER_PATH_URL } from '@/constants/routes';
 import useLoginForm from '@/hooks/module/auth/useLoginForm';
 import { useTranslations } from 'next-intl';
-import Image from 'next/image';
 import Link from 'next/link';
 
 const LoginForm = () => {
@@ -21,8 +21,8 @@ const LoginForm = () => {
   const t = useTranslations();
 
   return (
-    <Form onSubmit={onSubmit} className="p-6 md:p-8" {...form}>
-      <Stack gap={6}>
+    <Form onSubmit={onSubmit} className="p-6 md:p-8 w-full flex items-center justify-center" {...form}>
+      <Stack gap={6} width="full">
         <Stack className="text-center" align="center">
           <Typography variant={'h3'} className="text-2xl font-bold">
             {t('auth.login.title')}
@@ -56,42 +56,50 @@ const LoginForm = () => {
               />
             )}
           />
-          <Link href={FORGOT_PASSWORD_PATH_URL}>
-            <Button variant="link" type="button" size="sm" className="px-0 text-foreground font-normal">
-              {t('auth.form.label.forgotPassword')}
-            </Button>
-          </Link>
+          {AUTH_CONFIG.forgotPassword ? (
+            <Link href={FORGOT_PASSWORD_PATH_URL}>
+              <Button variant="link" type="button" size="sm" className="px-0 text-foreground font-normal">
+                {t('auth.form.label.forgotPassword')}
+              </Button>
+            </Link>
+          ) : null}
         </Stack>
         <Button type="submit" className="w-full">
           {t('common.login')}
         </Button>
-        <Box className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-          <Typography as="span" className="bg-card text-muted-foreground relative z-10 px-2">
-            {t('auth.continueWith')}
-          </Typography>
-        </Box>
-        <Box className="grid grid-cols-3 gap-4">
-          <Button variant="outline" type="button" className="w-full">
-            <Image src={AppleBrand} alt="Apple" width={16} height={16} />
-            <span className="sr-only">Login with Apple</span>
-          </Button>
-          <Button variant="outline" type="button" className="w-full">
-            <Image src={GoogleBrand} alt="Google" width={16} height={16} />
-            <span className="sr-only">Login with Google</span>
-          </Button>
-          <Button variant="outline" type="button" className="w-full">
-            <Image src={MetaBrand} alt="Meta" width={16} height={16} />
-            <span className="sr-only">Login with Meta</span>
-          </Button>
-        </Box>
-        <Box className="text-center text-sm">
-          {t('auth.dontHaveAccount')}{' '}
-          <Link href={REGISTER_PATH_URL}>
-            <Button variant="link" type="button" size="sm" className="px-0 text-foreground font-normal">
-              {t('common.register')}
-            </Button>
-          </Link>
-        </Box>
+        {AUTH_CONFIG.socialMedia ? (
+          <>
+            <Box className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+              <Typography as="span" className="bg-card text-muted-foreground relative z-10 px-2">
+                {t('auth.continueWith')}
+              </Typography>
+            </Box>
+            <Box className="grid grid-cols-3 gap-4">
+              <Button variant="outline" type="button" className="w-full fill-primary">
+                <AppleBrand />
+                <span className="sr-only">Login with Apple</span>
+              </Button>
+              <Button variant="outline" type="button" className="w-full fill-primary">
+                <GoogleBrand />
+                <span className="sr-only">Login with Google</span>
+              </Button>
+              <Button variant="outline" type="button" className="w-full fill-primary">
+                <MetaBrand />
+                <span className="sr-only">Login with Meta</span>
+              </Button>
+            </Box>
+          </>
+        ) : null}
+        {AUTH_CONFIG.signUp ? (
+          <Box className="text-center text-sm">
+            {t('auth.dontHaveAccount')}{' '}
+            <Link href={REGISTER_PATH_URL}>
+              <Button variant="link" type="button" size="sm" className="px-0 text-foreground font-normal">
+                {t('common.register')}
+              </Button>
+            </Link>
+          </Box>
+        ) : null}
       </Stack>
     </Form>
   );
