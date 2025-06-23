2c16508dd0ac39ec3de4a1eb05d04b2afcbdd530
arubaya <abi.arubaya@gmail.com>
2025-06-24 02:48:49 +0700

    feat(layout): add RootAuthenticatedLayout and DashboardPageRoute components for authenticated user experience

---
+++ app/(authenticated)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+// import DashboardPage from '@/components/page/dashboard/DashboardPage';
+
+const DashboardPageRoute = () => {
+  //   return <DashboardPage />;
+  return <div>DashboardPage</div>;
+};
+
+export default DashboardPageRoute;
