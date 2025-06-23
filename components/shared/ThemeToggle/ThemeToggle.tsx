3244c64bc8b21c3d38233c0f89002a30d73f5fa2
arubaya <abi.arubaya@gmail.com>
2025-06-24 02:46:44 +0700

    feat(ui): enhance ThemeToggle with translations and improved dropdown items

--- components/shared/ThemeToggle/ThemeToggle.tsx
+++ components/shared/ThemeToggle/ThemeToggle.tsx
@@ -1,27 +1,57 @@
 'use client';
 
 import { Moon, Sun } from 'lucide-react';
+import { useTranslations } from 'next-intl';
 import { useTheme } from 'next-themes';
 
 import Button from '@/components/ui/Button';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/DropdownMenu';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '@/components/ui/DropdownMenu';
 
-function ThemeToggle() {
-  const { setTheme } = useTheme();
+interface ThemeToggleProps {
+  children?: React.ReactNode;
+}
+
+export const ThemeIcon = () => {
+  const t = useTranslations();
+  return (
+    <>
+      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <span className="sr-only">{t('common.theme.title')}</span>
+    </>
+  );
+};
+
+function ThemeToggle({ children }: ThemeToggleProps) {
+  const { setTheme, theme } = useTheme();
+  const t = useTranslations();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
+        {children ? (
+          children
+        ) : (
+          <Button variant="ghost" size="icon">
+            <ThemeIcon />
+          </Button>
+        )}
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
+      <DropdownMenuContent align="start" side="right">
+        <DropdownMenuCheckboxItem checked={theme === 'light'} onCheckedChange={() => setTheme('light')}>
+          {t('common.theme.light')}
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem checked={theme === 'dark'} onCheckedChange={() => setTheme('dark')}>
+          {t('common.theme.dark')}
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem checked={theme === 'system'} onCheckedChange={() => setTheme('system')}>
+          {t('common.theme.system')}
+        </DropdownMenuCheckboxItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
