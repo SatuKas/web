32c8cb434d7eb457b3847118af43483db8be9d11
arubaya <abi.arubaya@gmail.com>
2025-06-24 02:47:36 +0700

    feat(hooks): add useIsMobile hook for responsive design

--- types/client/ui.ts
+++ types/client/ui.ts
@@ -1,3 +1,5 @@
+import { TooltipContent } from '@/components/ui/Tooltip/Tooltip';
+
 export type LabelDirection = 'vertical' | 'horizontal';
 
 export type DefaultInputProps = {
@@ -7,3 +9,56 @@ export type DefaultInputProps = {
   description?: string;
   required?: boolean;
 };
+
+export type SidebarMenuActionItem = {
+  title: SidebarTitle;
+  url?: string;
+  icon?: React.ReactNode;
+  tooltip?: string | React.ComponentProps<typeof TooltipContent>;
+};
+
+export type SidebarSubMenuItem = {
+  title: SidebarTitle;
+  url?: string;
+  icon?: React.ReactNode;
+  tooltip?: string | React.ComponentProps<typeof TooltipContent>;
+  action?: SidebarMenuActionItem[];
+};
+
+export type SidebarMenuItem = {
+  title: SidebarTitle;
+  url?: string;
+  icon?: React.ReactNode;
+  isActive?: boolean;
+  tooltip?: string | React.ComponentProps<typeof TooltipContent>;
+  subMenu?: SidebarSubMenuItem[];
+  action?: SidebarMenuActionItem[];
+};
+
+export type SidebarOptionalMenuItem = {
+  title: SidebarTitle;
+  url?: string;
+  icon?: React.ReactNode;
+  isActive?: boolean;
+  tooltip?: string | React.ComponentProps<typeof TooltipContent>;
+};
+
+export type SidebarMenuGroup = {
+  title?: SidebarTitle;
+  menu: SidebarMenuItem[];
+};
+
+export type SidebarMenu = SidebarMenuGroup[];
+
+export enum SidebarTitle {
+  DASHBOARD = 'dashboard',
+  SETTINGS = 'settings',
+  PROFILE = 'profile',
+  LOGOUT = 'logout',
+  HELP = 'help',
+  ABOUT = 'about',
+  CONTACT = 'contact',
+  TERMS = 'terms',
+  PRIVACY = 'privacy',
+  THEME = 'theme',
+}
