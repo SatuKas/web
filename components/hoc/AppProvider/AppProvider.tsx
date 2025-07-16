fdd23c73ab1fe533706e9d706c035a8c0f9ef60c
arubaya <abi.arubaya@gmail.com>
2025-07-16 11:32:02 +0700

    feat(hoc): wrap children with ToastProvider and QueryProvider for enhanced context

--- components/hoc/AppProvider/AppProvider.tsx
+++ components/hoc/AppProvider/AppProvider.tsx
@@ -1,11 +1,17 @@
 import { PropsWithChildren } from 'react';
+import QueryProvider from '../QueryProvider';
 import ThemeProvider from '../ThemeProvider';
+import ToastProvider from '../ToastProvider';
 import TranslationProvider from '../TranslationProvider';
 
 const AppProvider = ({ children }: PropsWithChildren) => {
   return (
     <ThemeProvider>
-      <TranslationProvider>{children}</TranslationProvider>
+      <ToastProvider>
+        <QueryProvider>
+          <TranslationProvider>{children}</TranslationProvider>
+        </QueryProvider>
+      </ToastProvider>
     </ThemeProvider>
   );
 };
