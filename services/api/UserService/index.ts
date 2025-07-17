664a53d527e9cbf0151a591f589c29ebd2856f83
arubaya <abi.arubaya@gmail.com>
2025-07-18 00:48:58 +0700

    feat(api): add UserService for user-related API requests and define user routes

---
+++ services/api/UserService/index.ts
@@ -0,0 +1,15 @@
+import { BaseHttpClient } from '@/services/api/core/BaseHttpClient';
+import { USER_CURRENT_PATH } from '@/services/api/routes';
+import { ApiResponse } from '@/types/api/common';
+import { UserResponse } from '@/types/api/user';
+
+/**
+ * UserService handles user-related API requests.
+ * Inherits HTTP methods from BaseHttpClient.
+ */
+export class UserService extends BaseHttpClient {
+  async getCurrentUser() {
+    // GET request to get current user endpoint
+    return this.get<ApiResponse<UserResponse>>(USER_CURRENT_PATH).then((res) => res.data);
+  }
+}
--- services/api/routes/index.ts
+++ services/api/routes/index.ts
@@ -17,3 +17,7 @@ export const AUTH_REFRESH_TOKEN_PATH = `${AUTH_PATH}/refresh-token`;
 export const AUTH_VERIFY_EMAIL_PATH = `${AUTH_PATH}/verify-email/:token`;
 export const AUTH_FORGOT_PASSWORD_PATH = `${AUTH_PATH}/forgot-password`;
 export const AUTH_RESET_PASSWORD_PATH = `${AUTH_PATH}/reset-password/:token`;
+
+// User Path
+export const USER_PATH = '/user';
+export const USER_CURRENT_PATH = `${USER_PATH}/me`;
