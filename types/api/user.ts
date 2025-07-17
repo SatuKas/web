f1f201599702468641b43788ea80a8f26e32ab0c
arubaya <abi.arubaya@gmail.com>
2025-07-18 00:47:59 +0700

    feat(types): add UserResponse and UserData types for user management

---
+++ types/api/user.ts
@@ -0,0 +1,10 @@
+export type UserResponse = {
+  id: string;
+  email: string;
+  name: string;
+  username: string;
+  role: string;
+  is_verified: boolean;
+  verified_at: string;
+  created_at: string;
+};
---
+++ types/client/user.ts
@@ -0,0 +1,10 @@
+export type UserData = {
+  id: string;
+  email: string;
+  name: string;
+  username: string;
+  role: string;
+  isVerified: boolean;
+  verifiedAt: string;
+  createdAt: string;
+};
