684db89ca9fda3321c224f764409c68ba5b7519b
arubaya <abi.arubaya@gmail.com>
2025-07-23 23:30:16 +0700

    refactor(metadata): consolidate app title and update icon references for improved clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,20 +15,22 @@ const geistMono = Geist_Mono({
   subsets: ['latin'],
 });
 
+const appTitle = `${APP_NAME} | ${APP_DESCRIPTION}`;
+
 export const metadata: Metadata = {
-  title: APP_NAME,
+  title: appTitle,
   description: APP_DESCRIPTION,
   icons: {
-    icon: APP_LOGO.icon,
+    icon: APP_LOGO.icon.square,
   },
   openGraph: {
-    title: APP_NAME,
+    title: appTitle,
     description: APP_DESCRIPTION,
     images: [APP_IMAGE_META],
   },
   twitter: {
     card: 'summary_large_image',
-    title: APP_NAME,
+    title: appTitle,
     description: APP_DESCRIPTION,
     images: [APP_IMAGE_META],
   },
