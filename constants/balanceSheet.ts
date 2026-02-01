3d7ecea64a24c24ede42b9439b203561aefa25b5
arubaya <abi.arubaya@gmail.com>
2026-02-01 14:36:05 +0700

    fix(balanceSheet): update default date value in BALANCE_SHEET_FILTER_FORM_DEFAULT_VALUES from undefined to an empty string

--- constants/balanceSheet.ts
+++ constants/balanceSheet.ts
@@ -1,5 +1,5 @@
 import { BalanceSheetFilterFormData } from '@/types/client/report';
 
 export const BALANCE_SHEET_FILTER_FORM_DEFAULT_VALUES: BalanceSheetFilterFormData = {
-  date: undefined,
+  date: '',
 };
